1c98ce574f90b9b1a1a90314e29ff8b1dc19698b
SHINJI YANASEKO <167289098+UMIZORA@users.noreply.github.com>
2025-04-11 22:53:03 +0900

    🐛 /もいらない

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,5 @@
 const isProd = process.env.NODE_ENV === 'production';
-const basePath = isProd ? '/' : '';
+const basePath = isProd ? '' : '';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
